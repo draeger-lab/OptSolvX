79e654191b8df2bc12333d6b83d5f2ce542e4cb9
xts-Michi <xthebram@web.de>
2025-07-04 16:04:53 +0200

    fix: fixed an error in the Variable Bounds

--- src/main/java/org/optsolvx/solver/CommonsMathSolver.java
+++ src/main/java/org/optsolvx/solver/CommonsMathSolver.java
@@ -4,65 +4,127 @@ import org.apache.commons.math3.optim.linear.*;
 import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
 import org.apache.commons.math3.optim.PointValuePair;
 import org.apache.commons.math3.optim.MaxIter;
+import javax.sound.sampled.Line;
 import java.util.*;
 
+/**
+ * Adapter Implementation of ILPSolver using Apache Commons Math as backend.
+ * Encapsulates the SimplexSolver for linear programs (LP), mapping OptSolvX API
+ * calls to the Common Math solver structures. This implementation covers
+ * standard LPs but has some limitations in variable bounds (see below).
+ */
 public class CommonsMathSolver implements ILPSolver {
-    // Internal fields
+
+    // Number of decision variables in the model
     private int numVars;
+    // Objective function coefficients (size = numVars)
     private double[] objectiveCoeffs;
+    // Flag indicating maximize (true) or minimize (false)
     private boolean maximize;
+    // List of all linear constraints in the model
     private List<LinearConstraint> constraints = new ArrayList<>();
+    // Per-variable lower and upper bounds (size = numVars)
     private double[] lowerBounds;
     private double[] upperBounds;
 
+    /**
+     * Initializes the solver for a new problem instance.
+     * Resets constraints and sets default bounds (lower = 0, upper = +Inf).
+     * @param numVars Number of variables for the problem
+     */
     @Override
     public void initializeModel(int numVars) {
         this.numVars = numVars;
         this.lowerBounds = new double[numVars];
         this.upperBounds = new double[numVars];
-        Arrays.fill(this.lowerBounds, 0);
-        Arrays.fill(this.upperBounds, Double.POSITIVE_INFINITY);
+        Arrays.fill(this.lowerBounds, 0); // Standard LP: all variables >= 0
+        Arrays.fill(this.upperBounds, Double.POSITIVE_INFINITY); // No explicit upper bound
     }
 
+    /**
+     * Defines the objective function to the optimized
+     * @param coefficients Coefficient array, same order as variables
+     * @param maximize True or maximization, false for minimization
+     */
     @Override
     public void setObjective(double[] coefficients, boolean maximize) {
         this.objectiveCoeffs = coefficients;
         this.maximize = maximize;
     }
 
+    /**
+     * Adds a linear constraint to the model.
+     * @param coefficients Coefficient array for the constraint (length = numVars)
+     * @param relationship Relationship of the constraint (LEQ, GEQ, EQ)
+     * @param value Right-hand side value for the constraint
+     */
     @Override
     public void addConstraint(double[] coefficients, Relationship relationship, double value) {
         constraints.add(new LinearConstraint(coefficients, relationship, value));
     }
 
+    /**
+     * Sets explicit lower and upper bounds for a variable.
+     * NOTE: Commons Math SimplexSolver only natively supports non-negativity;
+     * for general bounds, these would need to be transformed into additional constraints.
+     * @param varIndex Index of the variable (0-based)
+     * @param lower Lower bound
+     * @param upper Upper bound
+     */
     @Override
     public void setVariableBounds(int varIndex, double lower, double upper) {
         this.lowerBounds[varIndex] = lower;
         this.upperBounds[varIndex] = upper;
     }
 
+    /**
+     * Solves the current LP using Commons Math SimplexSolver.
+     * Variable bounds beyond non-negativity are NOT enforced unless added as constraints.
+     * Returns infeasible solution if any solver error occurs.
+     * @return LPSolution (variables, objective, feasible flag)
+     */
     @Override
     public LPSolution solve() {
-        // Using the Simplex solver
-        // TODO: Variable bounds einbauen!
+        // Work on a fresh constraint list to avoid duplicates.
+        List<LinearConstraint> allConstraints = new ArrayList<>(constraints);
+
+        // Add variable bounds as constraints
+        for (int i = 0; i < numVars; i++) {
+            double[] coeff = new double[numVars];
+            coeff[i] = 1.0; // Only x_i in this constraint
+            allConstraints.add(new LinearConstraint(coeff, Relationship.GEQ, lowerBounds[i]));
+            if (upperBounds[i] < Double.POSITIVE_INFINITY) {
+                allConstraints.add(new LinearConstraint(coeff, Relationship.LEQ, upperBounds[i]));
+            }
+        }
+
+        // Build objective function
         LinearObjectiveFunction objective = new LinearObjectiveFunction(objectiveCoeffs, 0);
+
+        // Solve the LP with all constraints (including bounds)
         SimplexSolver solver = new SimplexSolver();
         try {
             PointValuePair result = solver.optimize(
                     new MaxIter(100),
                     objective,
-                    new LinearConstraintSet(constraints),
+                    new LinearConstraintSet(allConstraints),
                     maximize ? GoalType.MAXIMIZE : GoalType.MINIMIZE,
                     new NonNegativeConstraint(true)
             );
-            double[] variables = result.getPoint();
+            // Extract variable values and objective
+            double[] vars = result.getPoint();
             double obj = result.getValue();
-            return new LPSolution(variables, obj, true);
+            return new LPSolution(vars, obj, true);
         } catch (Exception e) {
+            // If infeasible or error, return empty solution
             return new LPSolution(new double[numVars], Double.NaN, false);
         }
     }
 
+    /**
+     * Resets all model data for reuse of the solver instance.
+     * Does not change number of variables or bounds.
+     */
     @Override
     public void reset() {
         constraints.clear();
