b0f46d2c7352a6283bf53d410dedcb867741751b
xts-Michi <xthebram@web.de>
2025-07-20 13:26:44 +0200

    fix: renamed a wrong class name

--- src/main/java/org/optsolvx/solver/LPSolution.java
+++ src/main/java/org/optsolvx/solver/LPSolution.java
@@ -5,16 +5,16 @@ import java.util.Map;
 public class LPSolution {
 
     private final Map<String, Double> variableValues; // Variable name -> value
-    private final double objectValue;
+    private final double objectiveValue;
     private final boolean feasible; // true if solution is feasible
 
     public LPSolution(Map<String, Double> variableValues, double objectValue, boolean feasible) {
         this.variableValues = variableValues;
-        this.objectValue = objectValue;
+        this.objectiveValue = objectValue;
         this.feasible = feasible;
     }
 
     public Map<String, Double> getVariableValues() { return variableValues; }
-    public double getObjectValue() { return objectValue; }
+    public double getObjectiveValue() { return objectiveValue; }
     public boolean isFeasible() { return feasible; }
 }
