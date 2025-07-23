19276e93e68413f3597bd1ecddedd002f31a15e4
xts-Michi <xthebram@web.de>
2025-07-23 15:17:05 +0200

    doc: Changed the language of the JavaDoc in  SolverDemo

--- src/main/java/org/optsolvx/solver/SolverDemo.java
+++ src/main/java/org/optsolvx/solver/SolverDemo.java
@@ -5,26 +5,26 @@ import java.util.Map;
 
 public class SolverDemo {
     public static void main(String[] args) {
-        // 1. Modell aufbauen
+        // 1. Build the model
         AbstractLPModel model = new AbstractLPModel();
         model.addVariable("x", 0, Double.POSITIVE_INFINITY);
         model.addVariable("y", 0, Double.POSITIVE_INFINITY);
 
-        // Ziel: max x + y
+        // Goal: max x + y
         model.setObjective(Map.of("x", 1.0d, "y", 1.0d), OptimizationDirection.MAXIMIZE);
 
         // Constraints:
         model.addConstraint("c1", Map.of("x", 1.0d, "y", 2.0d), Constraint.Relation.LEQ, 4.0d);
         model.addConstraint("c2", Map.of("x", 1.0d), Constraint.Relation.LEQ, 3.0d);
 
-        // Modell finalisieren
+        // Finalize model
         model.build();
 
-        // 2. Solver benutzen
+        // 2. Use the solver
         CommonsMathSolver solver = new CommonsMathSolver();
         LPSolution solution = solver.solve(model);
 
-        // 3. Ergebnis ausgeben
+        // 3. Output result
         System.out.println("Variable values: " + solution.getVariableValues());
         System.out.println("Objective: " + solution.getObjectiveValue());
         System.out.println("Feasible: " + solution.isFeasible());
